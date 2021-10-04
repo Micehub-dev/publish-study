872fe03984bd58e6a580b89f891ffcbb551c81d7
sc.lee <sc.lee@micehub.com>
2021-10-04 16:39:08 +0900

    update: 코드 리팩토링, 반응형 대응, 각 슬라이드 높이 대응

--- sclee/carousel/script.js
+++ sclee/carousel/script.js
@@ -1,42 +1,68 @@
 function Carousel(selector){
-    var context = document.querySelector(selector);
+    this.context = document.querySelector(selector);
 
-    //context의 overflow 스타일을 hidden으로
-    context.style.overflow = "hidden";
+    this.init = function(){
+		//context의 overflow 스타일을 hidden으로
+		this.context.style.overflow = "hidden";
+
+		var children = this.context.children;   // context의 자식 HTML 태그들이 반환됨
+		// Type : HTMLCollection -> Array 형식
+		// children[0], children[1] ... HTML 태그를 가져옴
+		// children.length => 전체 크기를 알아낼 수 있음
+
+		this.slides = [];
+		this.slideSize = children.length;    // 슬라이드 페이지 개수
+
+        this.calculateContext();
+
+        // children 안에 있는 HTML 태그들을
+        // slides 안에 저장한다.
+        for(var i = 0; i < this.slideSize; ++i){
+            var slide = children[i];
+            this.slides.push(slide);
+        }
+
+        this.buildTrack();
+        this.bindEvents();
+    },
+
+    this.calculateContext = function(){
+        this.slideWidth = this.context.clientWidth;
+    };
+
+    this.buildTrack = function(){
+        // 슬라이드 트랙 생성
+        this.track = document.createElement("div");
+        this.track.style.position = "relative";
+        this.track.style.transform = "translate(0px, 0px)";
+        this.track.style.transition = "transform 300ms, height 300ms";
+        
+        this.setSlidePositions(true);
     
-    var children = context.children;   // context의 자식 HTML 태그들이 반환됨
-    // Type : HTMLCollection -> Array 형식
-    // children[0], children[1] ... HTML 태그를 가져옴
-    // children.length => 전체 크기를 알아낼 수 있음
+        // track을 context의 자식 요소로 배치
+        this.context.appendChild(this.track);
+        
+        this.setTrackPosition();
+    };
 
-    var slides = [];
-    var slideSize = children.length;    // 슬라이드 페이지 개수
+    this.setSlidePositions = function(initialize){
+        for(var i = 0; i < this.slideSize; ++i){
+            var slide = this.setSlidePosition(this.slides[i], i);
+            // slide를 track으로 이동
+            if(initialize){
+                this.track.appendChild(slide);
+            }
+        }
+    };
 
-    var slideWidth = context.clientWidth;
-
-    // children 안에 있는 HTML 태그들을
-    // slides 안에 저장한다.
-    for(var i = 0; i < slideSize; ++i){
-        var slide = children[i];
-        slides.push(slide);
-    }
-
-    // 슬라이드 트랙 생성
-    var track = document.createElement("div");
-    track.style.position = "relative";
-    track.style.transform = "translate(0px, 0px)";
-    track.style.transition = "transform 300ms";
-    
-    for(var i = 0; i < slideSize; ++i){
-        var slide = slides[i];    // i 번째 slide HTML 요소
-
+    this.setSlidePosition = function(slide, i){
         slide.style.position = "absolute";
         slide.style.top = "0px";
-        slide.style.left = (slideWidth * i)+"px";
+        slide.style.left = (this.slideWidth * i)+"px";
 
         var boxSizing = css(slide, "box-sizing");
         if(boxSizing === 'border-box'){
-            slide.style.width = slideWidth + "px";
+            slide.style.width = this.slideWidth + "px";
 
         }else if(boxSizing === 'content-box'){
             // 슬라이드 가로 크기에서
@@ -47,16 +73,13 @@ function Carousel(selector){
             var borderLeft = css(slide, "border-left-width");
             var borderRight = css(slide, "border-right-width");
 
-            var width = slideWidth - (paddingLeft + paddingRight + borderLeft + borderRight);
+            var width = this.slideWidth - (paddingLeft + paddingRight + borderLeft + borderRight);
             slide.style.width = width + "px";
         }
 
-        // slide를 track으로 이동
-        track.appendChild(slide);
-    }
+        return slide;
+    };
 
-    // track을 context의 자식 요소로 배치
-    context.appendChild(track);
 
     // 현재 슬라이드 상태
     // 만약, 이 슬라이드 기능의 요구사항 중에
@@ -65,32 +88,71 @@ function Carousel(selector){
     // 그때 cursor를 객체의 노출된 변수로 지정해야 될 필요가 생기는 것
     var cursor = 0;
     this.next = function(){
-        cursor = Math.min(cursor + 1, slideSize - 1);
-        track.style.transform = "translate(-"+(cursor*slideWidth)+"px, 0px)";
+        cursor = Math.min(cursor + 1, this.slideSize - 1);
+        this.setTrackPosition();
     }
 
     this.previous = function(){
         cursor = Math.max(cursor - 1, 0);
-        track.style.transform = "translate(-"+(cursor*slideWidth)+"px, 0px)";
+        this.setTrackPosition();
     }
 
     // cursor에 값만 알 수 있게 해주고
     // cursor를 임의로 바꿀 수 없게 해주세요.
     this.getSlideNumber = function(){
         return cursor;
+    };
+
+    this.setTrackPosition = function(){
+        this.track.style.transform = "translate(-"+(cursor * this.slideWidth)+"px, 0px)";
+        this.track.style.height = this.getCurrentSlideHeight() + "px";
+    };
+
+    this.getCurrentSlideHeight = function(){
+        return this.slides[cursor].clientHeight;
+    }
+
+    this.bindEvents = function(){
+        var carousel = this;
+        window.addEventListener("resize", function(e){
+            carousel.calculateContext();
+            carousel.setSlidePositions();
+            carousel.setTrackPosition();
+        });
+
+        var imgs = carousel.context.querySelectorAll("img");
+        var imgCount = 0;
+        imgs.forEach(function(img){
+            imgCount += img.complete ? 0 : 1;
+            img.onload = function(e){
+                notifyImgLoaded();
+            };
+        });
+
+        function notifyImgLoaded(){
+            imgCount--;
+            if(imgCount == 0){
+                carousel.setTrackPosition();
+            }
+        }
     }
     
-}
+    this.init();
+	
+	/**
+	 * Utility Functions
+	 */
+	function css(element, cssProperty){
+		var cssValue = window.getComputedStyle(element)[cssProperty];
+		// 예) cssValue <= "20px"
+		// "20px", "-30px", "100.56px"
+		// 정규식으로 숫자부분만 추출하기
+		var match = cssValue.match(/^(-?[0-9]+(\.[0-9]+)?)px$/);
+		if(match != null && match[1]){
+			return Number(match[1])
+		}
 
-function css(element, cssProperty){
-    var cssValue = window.getComputedStyle(element)[cssProperty];
-    // 예) cssValue <= "20px"
-    // "20px", "-30px", "100.56px"
-    // 정규식으로 숫자부분만 추출하기
-    var match = cssValue.match(/^(-?[0-9]+(\.[0-9]+)?)px$/);
-    if(match != null && match[1]){
-        return Number(match[1])
-    }
+		return cssValue;
+	}
 
-    return cssValue;
 }
